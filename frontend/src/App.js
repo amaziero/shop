67a6aa76845fe27bdc019ca86468be6b6aaff1ba
Alison Maziero <alison_lens@hotmail.com>
2021-11-25 21:14:49 -0300

    feat: created product screen and implemented styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import Footer from "./components/Footer"
 import Header from "./components/Header"
-import HomeScreen from "./screens/HomeScrean"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      <Header />
-        <main className='py-3'>
-          <Container >
-            <HomeScreen />
-          </Container>
-        </main>
-      <Footer />
-    </>
+    <Router >
+      <>
+        <Header />
+          <main className='py-3'>
+            <Container >
+              <Route path='/' component={HomeScreen} exact />
+              <Route path='/product/:id' component={ProductScreen} />
+            </Container>
+          </main>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
