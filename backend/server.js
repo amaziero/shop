ded6a6557c84528c770500576aba18830cd14f5f
Alison Maziero <alison_lens@hotmail.com>
2021-12-05 15:36:54 -0300

    feat: errors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import conncetDB from './config/db.js'
+import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 import productsRouter from './routes/products.routes.js'
 
 dotenv.config()
@@ -11,5 +12,9 @@ const app = express()
 app.use('/api/products', productsRouter)
 
 const PORT = process.env.PORT || 5000
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
