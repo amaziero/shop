d7a2ab3f58773c5805e805bc31e1bdb62105abca
Alison Maziero <alison_lens@hotmail.com>
2021-12-08 21:44:33 -0300

    feat: login and register features fineshed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Footer from "./components/Footer"
 import Header from "./components/Header"
 import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
-import { Container } from 'react-bootstrap'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 function App() {
   return (
@@ -14,9 +16,11 @@ function App() {
         <Header />
           <main className='py-3'>
             <Container >
-              <Route path='/' component={HomeScreen} exact />
+              <Route path='/login' component={LoginScreen} />
+              <Route path='/register' component={RegisterScreen} />
               <Route path='/product/:id' component={ProductScreen} />
               <Route path='/cart/:id?' component={CartScreen} />
+              <Route path='/' component={HomeScreen} exact />
             </Container>
           </main>
         <Footer />
